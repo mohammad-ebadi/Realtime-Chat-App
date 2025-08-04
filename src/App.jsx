e65e598205ebc56f21f3b7f8dbe9977305d74040
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-04 17:58:38 +0330

    initialize the app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Flex, VStack } from '@chakra-ui/react'
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <VStack>
+      <h1>App component</h1>
+      </VStack>
+      
+    </div>
+  )
+}
+
+export default App
