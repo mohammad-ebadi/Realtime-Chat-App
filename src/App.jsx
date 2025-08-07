24fd7ca8f87e2df3932966e19ae924007c3b542e
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-07 12:08:04 +0330

    Modify some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import AuthPage from "./pages/auth/AuthPage.jsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/" element={<Navigate to={"/auth"}></Navigate>}></Route>
       <Route path="/auth" element={<AuthPage></AuthPage>}></Route>
       {/* <Route path="/:username" element={<Home></Home>}></Route> */}
     </Routes>
