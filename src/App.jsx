614afd8492bbbec6699d6d9e7d23913d8677dea2
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-07 16:41:24 +0330

    Modify some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import AuthPage from "./pages/auth/AuthPage.jsx";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, firestore } from "./configs/Firebase.js";
+import { useAuthStore } from "./stores/useAuthStore.js";
+import { doc, getDoc } from "firebase/firestore";
 
 function App() {
+  const setUser = useAuthStore((state) => state.setUser);
+  const clearUser = useAuthStore((state) => state.clearUser);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        try {
+          const userDocRef = doc(firestore, "users", user.uid);
+          const userDocSnap = await getDoc(userDocRef);
+          if (userDocSnap.exists()) {
+            const userData = userDocSnap.data();
+            setUser({
+              uid: user.uid,
+              email: user.email,
+              photoURL: user.photoURL,
+              username: userData.username || null,
+              profilePicURL: userData.profilePicURL || null,
+            });
+          } else {
+            setUser({
+              uid: user.uid,
+              email: user.email,
+              photoURL: user.photoURL,
+              username: null,
+              profilePicURL: null,
+            });
+          }
+        } catch (error) {
+          console.log(error);
+          clearUser();
+        }
+      } else {
+        clearUser();
+      }
+    });
+    return () => unsubscribe();
+  }, [setUser, clearUser]);
   return (
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/auth" element={<AuthPage></AuthPage>}></Route>
-      {/* <Route path="/:username" element={<Home></Home>}></Route> */}
     </Routes>
   );
 }
