b5dec521b5434665fc8c7be1f5d19847980a0413
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-07 17:19:45 +0330

    Modify some files

--- src/components/sideBar/EditProfile.jsx
+++ src/components/sideBar/EditProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Modal,
   ModalOverlay,
@@ -15,46 +15,52 @@ import {
   Flex,
   Text,
 } from "@chakra-ui/react";
-import { Edit } from '../../assets/Constants';
-
+import { Edit } from "../../assets/Constants";
+import { useAuthStore } from "../../stores/useAuthStore";
 
 function EditProfile() {
-      const { isOpen, onOpen, onClose } = useDisclosure();
+  const { user } = useAuthStore();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-         <Button onClick={onOpen} size={"xl"} _hover={{bg:"transparent" , transform: "scale(1.2)"}} bg={"none"}>
-          <Edit></Edit>
-        </Button>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Edit Profile</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <Flex alignItems={"center"} gap={5}  mb={5}>
-                <Avatar
-                  name="Dan Abrahmov"
-                  src="https://bit.ly/dan-abramov"
-                  size={"xl"}
-                />
-                <Input type="file" border={"none"} cursor={"pointer"}></Input>
-              </Flex>
-              <VStack align={"start"}>
-                <Text fontSize={14}>Current Username :</Text>
-                <Input placeholder="Usrename..."></Input>
-              </VStack>
-            </ModalBody>
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={onClose}>
-                Close
-              </Button>
-              <Button>Save</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+      <Button
+        onClick={onOpen}
+        size={"xl"}
+        _hover={{ bg: "transparent", transform: "scale(1.2)" }}
+        bg={"none"}
+      >
+        <Edit></Edit>
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Profile</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Flex alignItems={"center"} gap={5} mb={5}>
+              <Avatar
+                name="Dan Abrahmov"
+                src="https://bit.ly/dan-abramov"
+                size={"xl"}
+              />
+              <Input type="file" border={"none"} cursor={"pointer"}></Input>
+            </Flex>
+            <VStack align={"start"}>
+              <Text fontSize={14}>`Current Username :${user.username}`</Text>
+              <Input placeholder="Usrename..."></Input>
+            </VStack>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button>Save</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
-  )
+  );
 }
 
-export default EditProfile
+export default EditProfile;
