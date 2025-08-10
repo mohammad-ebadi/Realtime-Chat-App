6a490d3dc40dab8419a93ea3d8bcc8f19ced84d7
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-10 17:41:09 +0330

    Modify some files

--- src/components/sideBar/sideBarSearchBar/SearchResults.jsx
+++ src/components/sideBar/sideBarSearchBar/SearchResults.jsx
@@ -2,23 +2,27 @@ import { Avatar, Box, Flex, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 function SearchResults({ username, profilePicURL }) {
+  const handleSearchResult = () => {};
   return (
     <>
-    
-    <Box borderBottom={"1px solid black"} cursor={"pointer"} bg={"orange.400"}>
-      <Flex alignItems={"center"} gap={1}>
-        <Flex alignItems={"center"} p={1}>
-          <Avatar name={username} src={profilePicURL} />
-        </Flex>
+      <Box
+        borderBottom={"1px solid black"}
+        cursor={"pointer"}
+        bg={"orange.400"}
+        onClick={handleSearchResult}
+      >
+        <Flex alignItems={"center"} gap={1}>
+          <Flex alignItems={"center"} p={1}>
+            <Avatar name={username} src={profilePicURL} />
+          </Flex>
 
-        <VStack align={"start"}>
-          <Text fontSize={"15px"}>{username}</Text>
-          <Text fontSize={"12px"}>Hey whats up dud ? How are you doing?</Text>
-        </VStack>
-      </Flex>
-    </Box>
+          <VStack align={"start"}>
+            <Text fontSize={"15px"}>{username}</Text>
+            <Text fontSize={"12px"}>Hey whats up dud ? How are you doing?</Text>
+          </VStack>
+        </Flex>
+      </Box>
     </>
-    
   );
 }
 
