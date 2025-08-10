bdc505eac63ae71d7df8a91bc08dff34855ba6a2
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-10 11:48:26 +0330

    Modify some files

--- src/components/sideBar/SearchBar.jsx
+++ src/components/sideBar/SearchBar.jsx
@@ -1,33 +1,3 @@
-// import { Box, Button, Flex, Input, VStack } from "@chakra-ui/react";
-// import React from "react";
-// import { Search } from "../../assets/Constants";
-// import SearchResults from "./SearchResults";
-
-// function SearchBar() {
-//   return (
-//     <Box bg={"orange.300"} w={"full"} h={"auto"} >
-      
-//       <Flex alignItems={"center"} justifyContent={"space-between"}>
-//         <Flex>
-//           <Input placeholder="Search User..." border={"none"} borderRadius={0} focusBorderColor='transparent'></Input>
-          
-//         </Flex>
-//         <Flex>
-//             <Box bg={"transparent"}  _hover={{bg:"none", transform: "scale(1.2)"}} cursor={"pointer"} pr={3}><Search></Search></Box>
-//         </Flex>
-//       </Flex>
-        
-        
-        
-//         <SearchResults></SearchResults>
-      
-//     </Box>
-//   );
-// }
-
-// export default SearchBar;
-
-
 import React, { useState } from "react";
 import { Flex, Input } from "@chakra-ui/react";
 import { collection, query, where, getDocs, limit } from "firebase/firestore";
@@ -67,7 +37,7 @@ function SearchBar() {
   };
 
   return (
-    <Flex direction="column" gap={3}>
+    <Flex direction="column" gap={3} bg={"orange.300"}>
       <Input
         placeholder="Search username..."
         value={searchTerm}
