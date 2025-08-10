a817d50346a709f2819d38a53709c4df2e1e1c22
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-10 11:50:17 +0330

    Modify some files

--- src/components/sideBar/RecentUsers.jsx
+++ src/components/sideBar/RecentUsers.jsx
@@ -1,10 +1,11 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import RecentUsersChat from "./RecentUsersChat";
 
 function RecentUsers() {
   return (
     <Box bg={"purple.200"} w={"full"} maxH={"68vh"} overflow={"auto"}>
+      <Text color={"gray.200"}>Recent Chats</Text>
       <RecentUsersChat></RecentUsersChat>
       <RecentUsersChat></RecentUsersChat>
       <RecentUsersChat></RecentUsersChat>
