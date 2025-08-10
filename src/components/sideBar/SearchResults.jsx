718243b457123da6b23da5f5be72eceb6776e0c2
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-10 11:45:52 +0330

    Modify some files

--- src/components/sideBar/SearchResults.jsx
+++ src/components/sideBar/SearchResults.jsx
@@ -1,16 +1,16 @@
 import { Avatar, Box, Button, Flex, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
-function SearchResults() {
+function SearchResults({username,profilePicURL}) {
   return (
     <Box borderBottom={"1px solid black"} cursor={"pointer"} bg={"orange.600"}>
       <Flex alignItems={"center"} gap={1}>
         <Flex alignItems={"center"} p={1}>
-          <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
+          <Avatar name={username} src={profilePicURL} />
         </Flex>
 
         <VStack align={"start"}>
-          <Text fontSize={"10px"}>Username</Text>
+          <Text fontSize={"10px"}>{username}</Text>
           <Text fontSize={"12px"}>
             Hey whats up dud ? How are you doing?
           </Text>
