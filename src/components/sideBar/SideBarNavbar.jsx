09f9bc1ff19ce7beef7cbd672a27b8507b3b2cc2
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-10 12:15:16 +0330

    Modify some files

--- src/components/sideBar/SideBarNavbar.jsx
+++ src/components/sideBar/SideBarNavbar.jsx
@@ -13,7 +13,7 @@ function SideBarNavbar() {
       <Flex alignItems={"center"} justifyContent={"space-between"}>
         <Flex alignItems={"center"} gap={1} p={1}>
           <Avatar name={user.username} src={user?.profilePicURL}>
-            <AvatarBadge boxSize="1.25em" bg="green.500" ><Edit></Edit></AvatarBadge>
+            <AvatarBadge boxSize="1em"  ><Edit></Edit></AvatarBadge>
           </Avatar>
           <p>{user?.username}</p>
         </Flex>
