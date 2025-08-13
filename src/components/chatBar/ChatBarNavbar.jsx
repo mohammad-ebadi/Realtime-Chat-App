34cd7737eb88f036461a786aaa070a35a21348a2
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-13 13:36:58 +0330

    Modify some files

--- src/components/chatBar/ChatBarNavbar.jsx
+++ src/components/chatBar/ChatBarNavbar.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { Avatar, AvatarBadge, Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 function ChatBarNavbar() {
@@ -6,7 +6,9 @@ function ChatBarNavbar() {
     <Box bg={"#F9F7F7"} w={"full"} maxH={"73vh"} overflow={"auto"}>
       <Flex alignItems={"center"} justifyContent={"space-between"}>
         <Flex alignItems={"center"} gap={1} p={2}>
-          <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" border={"2px solid #112D4E"}/>
+          <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" border={"2px solid #112D4E"}>
+            <AvatarBadge boxSize="1.25em" bg="green.500" />
+          </Avatar>
           <Text color={"#112D4E"}>Username</Text>
         </Flex>
       </Flex>
