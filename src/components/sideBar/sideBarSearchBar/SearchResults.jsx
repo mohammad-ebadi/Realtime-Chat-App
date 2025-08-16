7f7e979ea91414c9992e10011628fb726f7ed800
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-16 16:09:46 +0330

    Modify some files

--- src/components/sideBar/sideBarSearchBar/SearchResults.jsx
+++ src/components/sideBar/sideBarSearchBar/SearchResults.jsx
@@ -1,40 +1,46 @@
 import { Avatar, Box, Flex, Text, VStack } from "@chakra-ui/react";
-import { doc , updateDoc , arrayUnion} from "firebase/firestore";
-import React from "react";
+import { doc, updateDoc, arrayUnion } from "firebase/firestore";
+import React, { useState } from "react";
 import { firestore } from "../../../configs/Firebase";
 import { useAuthStore } from "../../../stores/useAuthStore";
 import useAppToast from "../../../hooks/useAppToast";
 
 function SearchResults({ username, profilePicURL, searchedUserUid }) {
-  const toast = useAppToast()
+  const toast = useAppToast();
   const user = useAuthStore((state) => state.user);
-  
+  const [showResult, setShowResult] = useState(true);
+
   const handleSearchResult = async () => {
-    await updateDoc(doc(firestore, "userChats", user.uid), {searchedUserUids:arrayUnion(searchedUserUid) });
-    toast.success("Ok")
+    await updateDoc(doc(firestore, "userChats", user.uid), {
+      searchedUserUids: arrayUnion(searchedUserUid),
+    });
+    toast.success("Ok");
+    setShowResult(false);
   };
   return (
     <>
-      <Box
-        border={"none"}
-        cursor={"pointer"}
-        bg={"#DBE2EF"}
-        onClick={handleSearchResult}
-        w={"30vw"}
-      >
-        <Flex alignItems={"center"} gap={1}>
-          <Flex alignItems={"center"} p={1}>
-            <Avatar name={username} src={profilePicURL} />
-          </Flex>
+      {showResult && (
+        <Box
+          border={"none"}
+          cursor={"pointer"}
+          bg={"#DBE2EF"}
+          onClick={handleSearchResult}
+          w={"30vw"}
+        >
+          <Flex alignItems={"center"} gap={1}>
+            <Flex alignItems={"center"} p={1}>
+              <Avatar name={username} src={profilePicURL} />
+            </Flex>
 
-          <VStack align={"start"}>
-            <Text fontSize={"15px"} color={"#112D4E"}>
-              {username}
-            </Text>
-            <Text>{searchedUserUid}</Text>
-          </VStack>
-        </Flex>
-      </Box>
+            <VStack align={"start"}>
+              <Text fontSize={"15px"} color={"#112D4E"}>
+                {username}
+              </Text>
+              <Text>{searchedUserUid}</Text>
+            </VStack>
+          </Flex>
+        </Box>
+      )}
     </>
   );
 }
