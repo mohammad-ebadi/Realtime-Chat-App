c7a768f7a7e263b5499e0c4e764a462a88515a86
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-16 13:00:02 +0330

    Modify some files

--- src/components/authForm/GoogleAuth.jsx
+++ src/components/authForm/GoogleAuth.jsx
@@ -45,6 +45,8 @@ function GoogleAuth({ signIn }) {
           });
 
           toast.success("Account created successfully!");
+           // creating a new "userChats" collection in firestore
+                  await setDoc(doc(firestore, "userChats", user.uid), {})
         } else {
           // Existing user - get data from Firestore
           const existingUserData = userSnap.data();
