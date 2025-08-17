1e83669fbe3c9700a01e1c2c1b87bdcc05357b63
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-17 10:38:55 +0330

    Modify some files

--- src/components/chatBar/ChatBarWindow.jsx
+++ src/components/chatBar/ChatBarWindow.jsx
@@ -1,14 +1,15 @@
-import { Box, Flex, VStack } from '@chakra-ui/react'
-import React from 'react'
-import ChatHistory from './ChatHistory'
+import { Box, Flex, VStack } from "@chakra-ui/react";
+import React from "react";
+import ChatHistory from "./ChatHistory";
+import { useHandleSelectedUser } from "../../stores/useHandleSelectedUser";
 
 function ChatBarWindow() {
+  const { selectedUser } = useHandleSelectedUser();
   return (
     <Box bg={"#DBE2EF"} w={"full"} h={"82vh"} overflow={"auto"}>
-   
-      <ChatHistory></ChatHistory>
+      {selectedUser ? <ChatHistory></ChatHistory> : null}
     </Box>
-  )
+  );
 }
 
-export default ChatBarWindow
+export default ChatBarWindow;
